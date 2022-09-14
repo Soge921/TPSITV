b7b1f647b74c7a946fde94d8f3221899c7c88d4e
zakaria <zakaria.bongiovanni@itiscuneo.eu>
2022-09-14 12:44:01 +0200

    prima modifica: aggiunto user name

--- 01_UDP/client.py
+++ 01_UDP/client.py
@@ -3,15 +3,21 @@ from socket import AF_INET, SOCK_DGRAM, socket
 BUFFER_SIZE = 1024
 
 
-HOST = "192.168.95.255"
+
+HOST = "127.0.0.1"
 PORT = 5000
 
 def chatClient():
-    while True:
+    with socket(AF_INET,SOCK_DGRAM) as s:
+        user = input("inserire il tuo username:")
+        user = user.encode('utf8')
+        s.sendto(user, (HOST, PORT))
+    while True: 
         with socket(AF_INET,SOCK_DGRAM) as s:
             msg =input("inserie messaggio:")
             msg = msg.encode('utf8')
-            s.sendto(msg,(HOST,PORT))
+            s.sendto(msg, (HOST, PORT))
+            
 
     
 
--- 01_UDP/server.py
+++ 01_UDP/server.py
@@ -6,13 +6,22 @@ BUFFER_SIZE = 1024
 HOST = "0.0.0.0"
 PORT = 5000
 
+print("---STANZA PUBBLICA---")
+
 def chatServer():
-    while True:
-        with socket(AF_INET,SOCK_DGRAM) as s:
-            s.bind((HOST,PORT))
-            msg = s.recvfrom(BUFFER_SIZE)
-            msg = msg[0].decode('utf8')
-            print(msg)
+    with socket(AF_INET,SOCK_DGRAM) as s:
+        s.bind((HOST,PORT))
+        data = s.recvfrom(BUFFER_SIZE)
+        user = data[0].decode('utf8')
+    
+   
+    with socket(AF_INET,SOCK_DGRAM) as s:
+        s.bind((HOST,PORT))
+        while True:
+            data = s.recvfrom(BUFFER_SIZE)
+            msg = data[0].decode('utf8')
+            
+            print(f"{user}:{msg}")
 
     
 
