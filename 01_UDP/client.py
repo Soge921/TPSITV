f7e6167feb5edca6df2e193f52b932080fe3db69
zakaria <zakaria.bongiovanni@itiscuneo.eu>
2022-09-19 12:16:28 +0200

    reso struttralmnete migliore

--- 01_UDP/client.py
+++ 01_UDP/client.py
@@ -1,30 +1,27 @@
+from ast import main
 from socket import AF_INET, SOCK_DGRAM, socket 
-
 BUFFER_SIZE = 1024
+def letturaServer():
+    host=input("inserire l'host:")
+    port=int(input("inserire porta"))
+    return host,port
+def letturaUsername(HOST,PORT):
 
-
-
-HOST = "127.0.0.1"
-PORT = 5000
-
-def chatClient():
     with socket(AF_INET,SOCK_DGRAM) as s:
         user = input("inserire il tuo username:")
         user = user.encode('utf8')
         s.sendto(user, (HOST, PORT))
+def chatClient(HOST,PORT):
+    
     while True: 
         with socket(AF_INET,SOCK_DGRAM) as s:
             msg =input("inserie messaggio:")
             msg = msg.encode('utf8')
-            s.sendto(msg, (HOST, PORT))
-            
-
-    
-
-
-    
-
-
+            s.sendto(msg, (HOST, PORT))                
+def main():
+    host,port= letturaServer()
+    letturaUsername(host,port)
+    chatClient(host,port)
 
 if __name__ == "__main__":
-    chatClient()
+    main()
--- 01_UDP/server.py
+++ 01_UDP/server.py
@@ -1,20 +1,14 @@
 from socket import AF_INET, SOCK_DGRAM, socket 
-
 BUFFER_SIZE = 1024
-
-
-HOST = "0.0.0.0"
-PORT = 5000
-
-print("---STANZA PUBBLICA---")
-
-def chatServer():
+def letturaServer():
+    host=input("inserire l'host:")
+    port=int(input("inserire porta"))
+    return host,port
+def chatServer(HOST,PORT):
     with socket(AF_INET,SOCK_DGRAM) as s:
         s.bind((HOST,PORT))
         data = s.recvfrom(BUFFER_SIZE)
         user = data[0].decode('utf8')
-    
-   
     with socket(AF_INET,SOCK_DGRAM) as s:
         s.bind((HOST,PORT))
         while True:
@@ -22,15 +16,12 @@ def chatServer():
             msg = data[0].decode('utf8')
             
             print(f"{user}:{msg}")
-
-    
-
-
-    
-
-
+def main():
+    print("---STANZA PUBBLICA---")
+    host,port = letturaServer()
+    chatServer(host,port)
 
 if __name__ == "__main__":
-    chatServer()
+    main()
 
 
